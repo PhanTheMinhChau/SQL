93a9a2d392cdb02837646688658965ff97b9df38
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2022-02-23 21:33:51 +0700

    Create tablePHONG.sql

---
+++ 23-02-2022/tablePHONG.sql
@@ -0,0 +1,5 @@
+SELECT * FROM qlnv.phong;
+INSERT INTO `qlnv`.`phong` (`MAPHONG`, `TENPHONG`, `DIACHI`, `TEL`) VALUES ('HCA', 'Hành chính tổ hợp', '123, Láng Hạ, Đống Đa, Hà Nội', '04 8585793');
+INSERT INTO `qlnv`.`phong` (`MAPHONG`, `TENPHONG`, `DIACHI`, `TEL`) VALUES ('KDA', 'Kinh Doanh', '123, Láng Hạ, Đống Đa, Hà Nội', '04 8574943');
+INSERT INTO `qlnv`.`phong` (`MAPHONG`, `TENPHONG`, `DIACHI`, `TEL`) VALUES ('KTA', 'Kỹ thuật', '123, Láng Hạ, Đống Đa, Hà Nội', '04 9480485');
+INSERT INTO `qlnv`.`phong` (`MAPHONG`, `TENPHONG`, `DIACHI`, `TEL`) VALUES ('QTA', 'Quản trị', '123, Láng Hạ, Đống Đa, Hà Nội', '');
