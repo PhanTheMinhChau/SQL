1523afbfd172aa5312ff257fb3f35bbf519b10a0
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2022-02-23 21:30:51 +0700

    Create tableDMNN.sql

---
+++ 23-02-2022/tableDMNN.sql
@@ -0,0 +1,7 @@
+SELECT * FROM qlnv.dmnn;
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('1', 'Anh');
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('2', 'Nga');
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('3', 'Pháp');
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('4', 'Nhật');
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('5', 'Trung Quốc');
+INSERT INTO `qlnv`.`dmnn` (`MANN`, `TENNN`) VALUES ('6', 'Hàn Quốc');
