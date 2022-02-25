a99e2d6daf1db389950f08da54b3f593760da638
Phan Thế Minh Châu <73175450+PhanTheMinhChau@users.noreply.github.com>
2022-02-25 12:19:37 +0700

    Rename 23-02-2022/bai3/bai3.2.sql to 23-02-2022/bai/bai3.2.sql

--- 23-02-2022/bai3/bai3.2.sql
+++
@@ -1,1 +0,0 @@
-select tenphong, tennn, luong  from qlnv.nhanvien, qlnv.phong, qlnv.tdnn, qlnv.dmnn where (nhanvien.hoten ='Phan Thế Minh Châu') and (nhanvien.maphong = phong.maphong) and (tdnn.manv = nhanvien.manv) and (tdnn.mann = dmnn.mann);
